a50fb8e29e1afd822c3ff632672e7f6126e317cd
just-another-dude <49374459+just-another-dude@users.noreply.github.com>
2024-07-07 11:41:01 -0400

    Add pv_calculator

--- financial_calculator.py
+++ financial_calculator.py
@@ -15,7 +15,7 @@ def fv_calculator(n: int, i: float, pmt: float, set_type: str) -> float:
     if set_type == "begin":
         coefficient = (pow((1 + i_dec), n) - 1) / i_dec * (1 + i_dec)
     elif set_type == "end":
-        coefficient = (pow(1+i_dec, n) - 1) / i_dec
+        coefficient = (pow(1 + i_dec, n) - 1) / i_dec
     else:
         raise ValueError(f"Unsupported set value: {set_type}")
 
@@ -24,4 +24,29 @@ def fv_calculator(n: int, i: float, pmt: float, set_type: str) -> float:
     return fv
 
 
+def pv_calculator(n: int, i: float, pmt: float, set_type: str) -> float:
+    """
+    :param n: Integer representing the number of periods/intervals.
+    :param i: Float representing the interest rate.
+    :param pmt: Float representing the payment amount.
+    :param set_type: String representing the type of calculation.
+    :return: Float representing the present value.
+    """
+
+    i_dec = i / 100
+
+    if set_type == "begin":
+        coefficient = ((pow(1 + i_dec, n) - 1) / (i_dec * pow(1 + i_dec, n))) * (1 + i_dec)
+    elif set_type == "end":
+        coefficient = (pow(1 + i_dec, n) - 1) / (i_dec * pow(1 + i_dec, n))
+    else:
+        raise ValueError(f"Unsupported set value: {set_type}")
+
+    print(coefficient)
+    pv = pmt * coefficient
+
+    return pv
+
+
 print(fv_calculator(n=3, i=5, pmt=100, set_type="begin"))
+print(pv_calculator(n=8, i=3, pmt=1000, set_type="end"))
